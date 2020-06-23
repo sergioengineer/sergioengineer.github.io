9649393e3c6e51632061f0965471945ccd5db293
sergioengineer <sergiobg.engineer@gmail.com>
2020-06-23 09:41:23 -0300

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 
-let deferredPrompt;
+let dr;
 const banner = document.getElementById("banner");
 function showInstallPromotion()
 {
@@ -11,21 +11,21 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+ dr = e;
   // Update UI notify the user they can install the PWA
   showInstallPromotion();
 
   banner.addEventListener('click', (e) =>{
 
     banner.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
+   dr.prompt();
+   dr.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
       } else {
         console.log('User dismissed the A2HS prompt');
       }
-      deferredPrompt = null;
+     dr = null;
     });
   });
 
